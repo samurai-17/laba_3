242a1dff301d78fbf4ba0543f8ac3e0d0d910323
Samurai <dan931901@gmail.com>
2024-12-04 21:48:15 +0300

    добавление "интерфейса"

--- main.py
+++ main.py
@@ -2,29 +2,52 @@ import re
 import requests
 
 
-def check_the_string(text):
-    pattern = r"#[0-9a-fA-F]{6}\b"
-    answer = re.findall(pattern, text)
-    if not answer:
-        return f"Цветов не найдено"
-    return f"Все найденные цвета: {answer}"
+class MainProgram:
+
+    def check_the_string(self, text):
+        pattern = r"#[0-9a-fA-F]{6}\b"
+        answer = re.findall(pattern, text)
+        if not answer:
+            return f"Цветов не найдено"
+        return f"Все найденные цвета: {answer}"
+
+    def check_the_file(self, path):
+        try:
+            with open(path, encoding="utf-8") as file:
+                s = file.read()
+                return self.check_the_string(s)
+        except Exception as e:
+            return f"Ошибка открытия файла: {e}"
+
+    def check_the_url(self, url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return self.check_the_string(response.text)
+        except Exception as e:
+            return f"Ошибка чтения страницы: {e}"
+
+    def main_window(self):
+        t = 1
+        while t != 0:
+            print("Что вы хотите проверить?")
+            print("1 - строку", "\n2 - файл", "\n3 - url", "\n0 - Выход")
+            t = int(input())
+            if t == 1:
+                print("Введите строку")
+                ex = str(input())
+                print(self.check_the_string(ex))
+            elif t == 2:
+                print("Введите путь к файлу")
+                ex = str(input())
+                print(self.check_the_file(ex))
+            elif t == 3:
+                print("Введите url")
+                ex = str(input())
+                print(self.check_the_url(ex))
+            elif t == 4:
+                exit(0)
 
 
-def check_the_file(path):
-    try:
-        with open(path, encoding="utf-8") as file:
-            s = file.read()
-            return check_the_string(s)
-    except Exception as e:
-        return f"Ошибка открытия файла: {e}"
-
-
-def check_the_url(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return check_the_string(response.text)
-    except Exception as e:
-        return f"Ошибка чтения страницы: {e}"
-
-
+solution = MainProgram()
+solution.main_window()
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import unittest
-import main
+from main import *
 
 
 class TestCheckAll(unittest.TestCase):
@@ -11,16 +11,25 @@ class TestCheckAll(unittest.TestCase):
     def test_check_the_string(self):
         for i in self.valid_data:
             with self.subTest(i=i):
-                self.assertEqual(main.check_the_string(i), f"Все найденные цвета: ['{i}']")
+                self.assertEqual(MainProgram.check_the_string(solution, text=i), f"Все найденные цвета: ['{i}']")
 
     def test_check_the_string2(self):
         for i in self.invalid_data:
             with self.subTest(i=i):
-                self.assertEqual(main.check_the_string(i), "Цветов не найдено")
+                self.assertEqual(MainProgram.check_the_string(solution, text=i), "Цветов не найдено")
 
     def test_check_the_file(self):
-        self.assertEqual(main.check_the_file("example.txt"), "Все найденные цвета: ['#123456', '#aaafff', '#AAAFFF']")
+        self.assertEqual(MainProgram.check_the_file(solution, path="example.txt"),
+                         "Все найденные цвета: ['#123456', '#aaafff', '#AAAFFF']")
 
     def test_check_the_url(self):
         example_html = "<html><body>picker_wrapper{background:#f2f2f2}input:active{#1e90ff}</body></html>"
-        self.assertEqual(main.check_the_string(example_html), "Все найденные цвета: ['#f2f2f2', '#1e90ff']")
+        self.assertEqual(MainProgram.check_the_string(solution, text=example_html),
+                         "Все найденные цвета: ['#f2f2f2', '#1e90ff']")
+
+
+
+
+
+
+
