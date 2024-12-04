d392c430085cc07f2fbedf12f798109ce9e126a6
Samurai <dan931901@gmail.com>
2024-12-04 15:04:06 +0300

    добавление функции проверки страницы

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import re
+import requests
+
 
 def check_the_string(text):
     pattern = r"#[0-9a-fA-F]{6}\b"
     answer = re.findall(pattern, text)
-    return answer
+    if not answer:
+        return f"Цветов не найдено"
+    return f"Все найденные цвета: {answer}"
+
 
 def check_the_file(path):
-    file = open(path)
-    s = file.read()
-    return check_the_string(s)
+    try:
+        file = open(path, encoding="utf-8")
+        s = file.read()
+        return check_the_string(s)
+    except Exception as e:
+        return f"Ошибка открытия файла: {e}"
 
 
-t = 'example.txt'
-print(check_the_file(t))
+def check_the_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return check_the_string(response.text)
+    except Exception as e:
+        return f"Ошибка чтения страницы: {e}"
+
+
+print(check_the_url("https://findhow.org/5060-konverter-tsvetov.html"))
